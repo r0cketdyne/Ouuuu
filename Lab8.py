82a65c933191ae5fdc8932444722ec8ab8ccd192
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-07 16:11:16 -0500

    commented shit out

--- Lab8.py
+++ Lab8.py
@@ -1,25 +1,39 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Thu Mar  7 15:52:49 2024
-
-@author: stephenson
+Part IIa: Writing your own functions
+We will complete this program that requires the definition of a function named computeGCRatio to
+compute the GC content of a DNA molecule.
+Given a single string as input where each character is one of G, C, T or A, the GC ratio is computed as the
+count of G+C values over the total count of A,T,C,G values. It is often written in texts as the following,
+where each letter represents the count of instances in the string
 """
+# Lab 8
+# PUT YOUR DEFINITION OF THE FUNCTION computeGCRatio HERE!
+# Hint: your function will need 4 counter variables ( one for each
+# letter. And you will need to write a loop that steps through each
+# character in the string
+def computeGCRatio( # what is/are the parameters needed here?
+ # complete the logic here):
 
-import math
-x = 3.1;
-y = 1.0 - x;
 
-print("This evalues to ", math.fabs(y))
+
+
+#FUGAZZI
+#import math
+#x = 3.1;
+#y = 1.0 - x;
+
+#print("This evalues to ", math.fabs(y))
 #1 evaluated to 2.1 returns the absolute val of a num, as a float
 
-print("This evalues to ", math.floor(x))
+#print("This evalues to ", math.floor(x))
 #2 evaluates to 3. this particular method rounds a number DOWN to the nearest integer.
-print("This evalues to ", math.ceil(x))
+#print("This evalues to ", math.ceil(x))
 #this evaluates to 4. rounds up to nearest int
-print("This evaluates to", math.floor(y))
+#print("This evaluates to", math.floor(y))
 #this evaluates to -3 this, obviously then must round down to nearest int
-print("This evalues to", math.ceil(y))
+#print("This evalues to", math.ceil(y))
 #this evaluates to -2, and again, here it must again round up
 
 #biggest thing here. help(math) is a GREAT in console problem solving toolkit. 
+
+"""
