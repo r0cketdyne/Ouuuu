b9eb01c338ae84a768ddc111716fc03b00995d49
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-07 15:55:08 -0500

    flexible flave

---
+++ Lab8.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  7 15:52:49 2024
+
+@author: stephenson
+"""
+
+import math
+x = 3.1;
+y = 1.0 - x;
+
+print(math.fabs(y))
+#1 evaluated to 2.1
+
+print(math.floor(x))
+#2 evaluates to 3
