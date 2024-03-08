9ee5ac5f968032788a476936279d34e04ecbde5d
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-07 19:19:53 -0500

    it's functional, but need to fix the print statements. basically did this from scratch, just forgot that there was a count method.

--- Lab8.py
+++ Lab8.py
@@ -1,18 +1,7 @@
-
-###Part IIa: Writing your own functions
-###We will complete this program that requires the definition of a function named computeGCRatio to
-###compute the GC content of a DNA molecule.
-###Given a single string as input where each character is one of G, C, T or A, the GC ratio is computed as the
-###count of G+C values over the total count of A,T,C,G values. It is often written in texts as the following,
-###where each letter represents the count of instances in the string
-
-# Lab 8
-# PUT YOUR DEFINITION OF THE FUNCTION computeGCRatio HERE!
-# Hint: your function will need 4 counter variables ( one for each
-# letter. And you will need to write a loop that steps through each
-# character in the string
-
-#shit python is dynamically typed I don't need this
+#Matthew Stephenson
+#Lab8
+#3/7/2024
+#this program computes the GC content of a DNA molecule. 
 str_param = input("Dear Genome Sequencer, inject a string representing where each character is one of G, C, T or A, the GC ratio is computed as thecount of G+C values over the total count of A,T,C,G values\n")
 
 def computeGCRatio(str_param): # what is/are the parameters needed here?
@@ -20,7 +9,7 @@ def computeGCRatio(str_param): # what is/are the parameters needed here?
 #I think arrays are dynamically allocated at Python, else I would have allocated space for it below str_param
 #    print(rando_stackalloc)
     count1 = str_arr.count('A') #ok there's a literal count method
-    print(count1)
+    #print(count1)
     #Ouuu let's just see if anything is actually printed out. I'll inject a string
     #with A a few times and 
     count2 = str_arr.count('T') #use count method
@@ -31,7 +20,6 @@ def computeGCRatio(str_param): # what is/are the parameters needed here?
     #print(count4)
     print((count4 + count3) / (count1 + count2 + count4 + count3)) #implemented equation at canvas wireframe
 #    print(rando_stackalloc)
-
 
 computeGCRatio(str_param)
 #If you invoke a func, in python it seems like you also need to use it's affiliated param
