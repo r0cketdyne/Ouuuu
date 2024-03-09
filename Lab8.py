b866d26ffa8f5d367f8119c80aa33f76d307b3e7
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-09 00:57:10 -0500

    two minutes to spare

--- Lab8.py
+++ Lab8.py
@@ -1,60 +1,27 @@
 #Matthew Stephenson
 #Lab8
-#3/7/2024
+#3/9/2024
 #this program computes the GC content of a DNA molecule. 
-str_param = input("Dear Genome Sequencer, inject a string representing where each character is one of G, C, T or A, the GC ratio is computed as thecount of G+C values over the total count of A,T,C,G values\n")
 
-def computeGCRatio(str_param): # what is/are the parameters needed here?
-    str_arr = str_param #fuck, I forgot to att str to str_param. typos ffs
-#I think arrays are dynamically allocated at Python, else I would have allocated space for it below str_param
-#    print(rando_stackalloc)
-    count1 = str_arr.count('A') #ok there's a literal count method
-    #print(count1)
-    #Ouuu let's just see if anything is actually printed out. I'll inject a string
-    #with A a few times and 
-    count2 = str_arr.count('T') #use count method
-    #print(count2)
-    count3 = str_arr.count('C') #just use the count method 
-    #print(count3)
-    count4 = str_arr.count('G') #just going to use the count method
-    #print(count4)
-    print((count4 + count3) / (count1 + count2 + count4 + count3)) #implemented equation at canvas wireframe
-#    print(rando_stackalloc)
+str_param = input("Dear Genome Sequencer, inject a string representing where each character is one of G, C, T or A, the GC ratio is computed as the count of G+C values over the total count of A, T, C, G values\n")
+#at the line above you prompt the user for an input, and store that at the stack
+def computeGCRatio(str_param):
+#above you take in str_param as your parameter, which is what is currently at the stack and pass it into the func
+    str_arr = list(str_param)  # Fix the typo here, change 'str_arr = str_param' to 'str_arr = list(str_param)'
+    count1 = str_arr.count('A') #using the count method.
+    count2 = str_arr.count('T') #using the method again for 'T' at the stack location at str_param
+    count3 = str_arr.count('C') #using method again for 'C' at entire sequence
+    count4 = str_arr.count('G') #doing same thing as line above.
+    return (count3 + count4) / (count1 + count2 + count3 + count4)  # Corrected the formula
 
-computeGCRatio(str_param)
-#If you invoke a func, in python it seems like you also need to use it's affiliated param
-#not sure why. will research
-#chatbots are dumb. It couldn't even tell me
-#that I just needed to invoke this function. which I did at line 23. it was functional
+gcratio = computeGCRatio(str_param)  # Call the function and store the result in 'gcratio'
+#function has to be called, to actually get the little 'function machine' to start working.
+print("For the sequence:", str_param)  # Print the original DNA sequence
+print("The GC-Ratio is {:6.3f}".format(gcratio))  # Print the calculated GC ratio
 
 
 
-#in music, studio time is essential. faculty and student complaints, strikes. 
-#all people to at Concordia is protest. Have they tried 
-#putting in actual work lol. This isn't McGill. At least the campus is nice
-
-#FUGAZZI
-
-#gc_ratio = (count3 + count4) / (count1 + count2 + count3 + count4)  # Compute GC ratio
-#    print("GC Ratio:", gc_ratio)
-#two lines above are the ratio I need, but not implementing this yet. this
-#program is waaaay easier than I anticipated that it would be
-
-
-#import math
-#x = 3.1;
-#y = 1.0 - x;
-
-#print("This evalues to ", math.fabs(y))
-#1 evaluated to 2.1 returns the absolute val of a num, as a float
-
-#print("This evalues to ", math.floor(x))
-#2 evaluates to 3. this particular method rounds a number DOWN to the nearest integer.
-#print("This evalues to ", math.ceil(x))
-#this evaluates to 4. rounds up to nearest int
-#print("This evaluates to", math.floor(y))
-#this evaluates to -3 this, obviously then must round down to nearest int
-#print("This evalues to", math.ceil(y))
-#this evaluates to -2, and again, here it must again round up
-
-#biggest thing here. help(math) is a GREAT in console problem solving toolkit. 
+#ps: If you invoke a func, in python it seems like you also need to use it's affiliated param
+#I could not connect to Zoom earlier for the lab
+#I sent peter an email regarding this. I think it was related to me connection
+#this issue has since been fixed
